6c3aa5256eaabe225745faca138bc9a3c822d7bc
huan <hy30@st-andrews.ac.uk>
2020-04-30 23:03:02 +0100

    update

--- predict.py
+++ predict.py
@@ -1,48 +1,92 @@
-import xgboost as xgb
 import pandas as pd # analysis
 import numpy as np # calculation
 from pandas import Series, DataFrame
-from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_absolute_error
 from sklearn import ensemble
-import seaborn as sns
 
 # read data set
 data = pd.read_csv('data.csv') # read training data
-data = data.iloc[:,1:]
 
-# pick features that are most relevant
-k = 10
-cols = data.corr().nlargest(k, '日收益率_Dret')['日收益率_Dret'].index
-cm = np.corrcoef(data[cols].values.T)
-sns.set(font_scale=1.25)
-sns.heatmap(cm, cbar = True, annot=True, square=True, fmt='.2f',annot_kws={'size':10}, yticklabels=cols.values, xticklabels=cols.values)
+stocks = []
+stock_codes = list(data.iloc[:, 0].unique())
 
-data = data[['滞后一期收益', '市盈率_PE', '市净率_PB', '市销率_PS', '每股收益(摊薄)(元/股)_EPS', '每股营业利润(元/股)_OpPrfPS', '每股净资产(元/股)_NAPS', '每股营业收入_IncomePS']]
+for code in stock_codes:
+    df = data[data['股票代码_Stkcd'] == code]
+    stocks.append(df)
 
-# split into training set and test set
-# data.rename("")
-X = data.iloc[:, 1:]
-y = data.iloc[:, 0]
-X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.33)
+for i in range(len(stocks)):
+    data = stocks[i]
+    # delete the stock code
+    data = data.iloc[:,1:]
+    # split to x and y
+    X = data.iloc[:, 1:]
+    y = data.iloc[:, 0]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=False, test_size=0.2)
+    params = {'n_estimators': 500, 'max_depth': 6, 'min_samples_split': 2,
+            'learning_rate': 0.0001, 'loss': 'ls'}
+    clf = ensemble.GradientBoostingRegressor(**params)
 
-params = {'n_estimators': 500, 'max_depth': 6, 'min_samples_split': 2,
-          'learning_rate': 0.001, 'loss': 'ls'}
-clf = ensemble.GradientBoostingRegressor(**params)
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
 
-clf.fit(X_train, y_train)
-y_pred = clf.predict(X_test)
-mse = mean_squared_error(y_test, y_pred)
-print("MSE: %.6f" % mse)
+    print('-----------------------------------')
+    print('Stock Code: ', stock_codes[i])
 
-from sklearn.metrics import mean_absolute_error
-me = mean_absolute_error(y_test, y_pred)
-print('me: ', me)
+    mse = mean_squared_error(y_test, y_pred)
+    print("MSE: %.6f" % mse)
 
-# d = {'y': y_test, 'y_pred': y_pred, 'diff': abs(y_test - y_pred)}
-# df = pd.DataFrame(data=d)
-# temp = df[df['diff'] < me]
-# print('accuracy', (len(temp)/ len(y_test)) * 100)
+    me = mean_absolute_error(y_test, y_pred)
+    print('me: ', me)
+
+    '''
+    -----------------------------------
+Stock Code:  672
+MSE: 0.000864
+me:  0.022254277293874802
+-----------------------------------
+Stock Code:  63
+MSE: 0.002332
+me:  0.035109195328277346
+-----------------------------------
+Stock Code:  2302
+MSE: 0.000830
+me:  0.019881888097589197
+-----------------------------------
+Stock Code:  2307
+MSE: 0.001288
+me:  0.02550267491394151
+-----------------------------------
+Stock Code:  2773
+MSE: 0.000897
+me:  0.02380382597393028
+-----------------------------------
+Stock Code:  2792
+MSE: 0.000489
+me:  0.016330879865239956
+-----------------------------------
+Stock Code:  2821
+MSE: 0.001109
+me:  0.02607645004735473
+-----------------------------------
+Stock Code:  600487
+MSE: 0.000935
+me:  0.02323362063247149
+-----------------------------------
+Stock Code:  600535
+MSE: 0.000809
+me:  0.022143157406598807
+-----------------------------------
+Stock Code:  600545
+MSE: 0.000132
+me:  0.008671967475127924
+-----------------------------------
+Stock Code:  600585
+MSE: 0.000580
+me:  0.018715383698890888
+-----------------------------------
+Stock Code:  600801
+MSE: 0.000806
+me:  0.02026521755367372
+    '''
